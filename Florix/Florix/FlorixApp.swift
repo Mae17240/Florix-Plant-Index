e0146c74228efb164ce5cb02a9819fd3f08d6852
Mae17240 <joshuamae01@gmail.com>
2026-01-07 12:54:35 +0000

    Init Comit
    IOS - Setup

---
+++ Florix/Florix/FlorixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlorixApp.swift
+//  Florix
+//
+//  Created by Joshua Mae on 07/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FlorixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Florix/FlorixTests/FlorixTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlorixTests.swift
+//  FlorixTests
+//
+//  Created by Joshua Mae on 07/01/2026.
+//
+
+import Testing
+@testable import Florix
+
+struct FlorixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
