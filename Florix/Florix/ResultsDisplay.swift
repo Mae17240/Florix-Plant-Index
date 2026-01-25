86e903b49d6b4702a71c79a28df9ed3001db2e63
Mae17240 <joshuamae01@gmail.com>
2026-01-25 18:02:06 +0000

    Results page
    layout fix, background image and big box positions fixed and added with colour scheme.

--- Florix/Florix/ResultsDisplay.swift
+++ Florix/Florix/ResultsDisplay.swift
@@ -13,9 +13,40 @@ struct ResultsDisplay: View {
         ZStack {
             Color(red: 233/255, green: 224/255, blue: 207/255).opacity(1)
                 .ignoresSafeArea()
-            Text("Results Display")
-                .font(.title)
-                .padding()
+            VStack(spacing: 24) {
+                // Top image
+                ZStack(alignment: .topLeading) {
+                    Image("MainHome")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 280)
+                        .clipped()
+                        .ignoresSafeArea(edges: .top)
+                    VStack {
+                        Text("Orchid.")
+                            .font(.custom("Baskervville-Regular", size: 40))
+                            .foregroundColor(.white)
+                            .padding(.leading, 45)
+                            .padding(.top, 40)
+                            .padding(.bottom, -10)
+                        
+                        Text("Cattlea")
+                            .foregroundColor(.white)
+                            .font(.custom("DMSansBold", size: 20))
+                            .padding(.leading, -15)
+                            
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 280, alignment: .topLeading)
+                }
+                // Cream Rectangle
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color(red: 233/255, green: 224/255, blue: 207/255).opacity(1))
+                    .clipShape(TopCornersRounded(radius: 80))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+                    .padding(.top, -140) // overlap
+                    .ignoresSafeArea(edges: .bottom)
+            }
         }
     }
 }
